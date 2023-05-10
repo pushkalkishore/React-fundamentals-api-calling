5bb31bd98e610ddf0b23f0389fac3293c03ba230
pushkalkishore <kpushkal122@gmail.com>
2023-05-10 13:31:24 +0530

    Updates

---
+++ precache-manifest.accdcc48ed59c142278745e15108a2ac.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3950c298bae6058d6d6f508fe432c5b",
+    "url": "/React-fundamentals-api-calling/index.html"
+  },
+  {
+    "revision": "9a17bba82bdf0bbb3fdc",
+    "url": "/React-fundamentals-api-calling/static/js/2.4560719b.chunk.js"
+  },
+  {
+    "revision": "7f39e8f4ff4626882bee",
+    "url": "/React-fundamentals-api-calling/static/js/main.23ed33c5.chunk.js"
+  },
+  {
+    "revision": "786516e741007f13928c",
+    "url": "/React-fundamentals-api-calling/static/js/runtime~main.f4a46079.js"
+  }
+]);
